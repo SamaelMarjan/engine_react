e2ac93aa6956f952fb488a8a1d27ca91f90b256a
Samael Marjan <samaaelmarjan@gmail.com>
2022-09-20 15:54:29 +0600

    footer-sep-20-15-54

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <div className='flex'>
+        <div>icon</div>
+        <div>flex1</div>
+        <div>flex2</div>
+        <div>flex3</div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
