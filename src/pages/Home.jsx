b42db11c27e98d642d50485a545e87c0176bffbf
Samael Marjan <samaaelmarjan@gmail.com>
2022-09-20 22:37:34 +0600

    home-sep-20-22-37

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,13 @@ import Im2 from '../assets/image 5.png';
 const Home = () => {
   return (
     <div className='w-[100%] h-[100%] flex flex-col'>
-      <div className='w-[90%] h-[700px] md:h-[350px] m-auto'>
-        <div className=''>
+      <div className='w-[90%] h-[700px] md:h-[350px] m-auto items-center justify-center'>
+        <div>
           <p className='w-[173px] h-[60px] mt-10 text-2xl font-normal text-[#666666] tracking-widest'>Lorem ipsum</p>
           <p className='w-[260px] h-[95px] text-7xl font-normal'>Engine</p>
           <p className='w-[451PX] h-[102px] text-xl font-normal text-[#666666] tracking-widest'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
         </div>
-        <img className='md:absolute w-[500px] md:w-[750px] h-[200px] md:h-[450px] right-[0] top-[60px]' src={Img} alt='engine' />
+        <img className='md:absolute w-[500px] md:w-[750px] h-[300px] md:h-[450px] right-[0] top-[60px]' src={Img} alt='engine' />
       </div>
       <div className='bg-[#10181B] w-[100%] h-[900px] md:h-[450px] '>
         <div className='w-[90%] h-[100%] m-auto  flex flex-col md:flex-row items-center '>
@@ -21,8 +21,8 @@ const Home = () => {
           </div>
           <div className='w-[60%] h-[50%] md:h-[100%]'>
             <div className='w-[100%] md:w-[600px] h-[100%] md:h-[200px] md:mt-[180px] ml-[50px]'>
-              <p className='w-[100%] md:w-[500px] h-[100px] text-[#fff] text-xl font-medium tracking-widest'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-              <p className='w-[100%] md:w-[100%] h-[100%] font-normal text-xl text-[#666666] tracking-widest'>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+              <p className='w-[100%] md:w-[500px] h-[100px] md:h-[80px] text-[#fff] text-xl font-medium tracking-widest'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+              <p className='w-[100%] md:w-[100%] h-[300px] md:h-[150px]  font-normal text-xl text-[#666666] tracking-widest'>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
             </div>
           </div>
         </div>
