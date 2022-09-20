22814eb742516d976e33b8f902f3ac3e4c2b9534
Samael Marjan <samaaelmarjan@gmail.com>
2022-09-20 17:22:45 +0600

    navbar-sep-20-17-22

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <div className='w-[100%] h-[100%]'>
+    <div className='w-[100%] h-[100%] relative'>
       <div className='w-[90%] h-[100%] m-auto'>
         <Navbar />
         <Main />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Img from '../assets/Figmateam.png';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-[100%] h-[80px] flex items-end'>
+      <img className='w-[255.2px] h-[24.64px]' src={Img} alt='logo' />
+    </div>
   );
 }
 
